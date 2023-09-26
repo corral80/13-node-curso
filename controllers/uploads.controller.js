7283acad8c2cac16a6e8e9bf8c3473d4b78299a1
Sergio Corral <sacorralo@falabella.cl>
2023-09-25 23:16:42 -0300

    proyecto completo, subida img local

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,4 +1,6 @@
 const { request, response } = require("express");
+const path = require('node:path')
+const fs = require('node:fs')
 
 const { subirArchivo } = require('../helpers');
 const { Usuario, Producto } = require("../models");
@@ -55,6 +57,16 @@ const actualizarImagen = async (req = request, res = response) =>{
             return res.status(500).json({ msg : 'Coleccion no permitida'})
     }
 
+    //Limpiar imagenes previas
+    if( modelo.img ){
+        //Hay que borrar la imagen del servidor
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img)
+        if ( fs.existsSync( pathImagen) ){
+            fs.unlinkSync( pathImagen )
+        }
+    }
+
+
     const nombre = await subirArchivo( req.files , undefined, coleccion)
     modelo.img = nombre
 
@@ -65,7 +77,57 @@ const actualizarImagen = async (req = request, res = response) =>{
 }
 
 
+const mostrarImagen = async (req = request, res = response)=>{
+
+    const { id, coleccion } = req.params
+
+    let modelo
+
+    switch ( coleccion ) {
+        case 'usuarios':
+        
+            modelo = await Usuario.findById(id)
+            if( !modelo ){
+                return res.status(400).json({
+                    msg : `No existe un usuario con el id ${id}`
+                })
+            }
+        break;
+
+        case 'productos':
+
+            modelo = await Producto.findById(id)
+            if( !modelo ){
+                return res.status(400).json({
+                    msg : `No existe un producto con el id ${id}`
+                })
+            }
+
+        break;
+
+        default:
+            return res.status(500).json({ msg : 'Coleccion no permitida'})
+    }
+
+    //Limpiar imagenes previas
+    if( modelo.img ){
+        //Hay que borrar la imagen del servidor
+        const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img )
+        if ( fs.existsSync( pathImagen) ){
+            
+            return res.sendFile( pathImagen )
+
+        }
+    }
+
+    const pathImagen = path.join( __dirname, '../assets','no-image.jpg' )
+
+    res.sendFile( pathImagen )
+    
+}
+
 module.exports = {
     cargarArchivo,
-    actualizarImagen
+    actualizarImagen,
+    mostrarImagen
 }
